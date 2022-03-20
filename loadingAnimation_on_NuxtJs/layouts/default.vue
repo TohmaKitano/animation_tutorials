af0ca0b5d39cb01394f273e539fd41922472ff9c
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-20 16:45:43 +0900

    【stack】can not resolve old gsap settings(TweenMax)

--- loadingAnimation_on_NuxtJs/layouts/default.vue
+++ loadingAnimation_on_NuxtJs/layouts/default.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import { TweenMax, Power3 } from 'gsap'
 import { mapGetters, mapActions } from 'vuex'
 import Logo from '~/components/Logo'
 export default {
@@ -35,69 +36,69 @@ export default {
         .matches
       if (!next && prev) {
         const winWidth = window.innerWidth
-        // GSAP.staggerFromTo(
-        //   '.Logo span',
-        //   1.5,
-        //   {
-        //     left: `${winWidth / 2}px`,
-        //     scale: 3,
-        //     opacity: 0,
-        //     rotationX: 180,
-        //     cycle: {
-        //       y: function(index) {
-        //         return `${Math.floor(Math.random() * 600 - 300)}px`
-        //       },
-        //       z: function(index) {
-        //         return `${winWidth - Math.floor(Math.random() * 40 - 20)}px`
-        //       },
-        //       left: function(index) {
-        //         return `${winWidth / 2 -
-        //           Math.floor(Math.random() * 600 - 300)}px`
-        //       }
-        //     }
-        //   },
-        //   {
-        //     scale: 1,
-        //     opacity: 1,
-        //     rotationX: 0,
-        //     rotationY: 360,
-        //     rotationZ: 360,
-        //     cycle: {
-        //       y: '0px',
-        //       z: '0px',
-        //       left: function(index) {
-        //         return `${winWidth / 2 -
-        //           168 / 2 +
-        //           index * 14 +
-        //           (isMobile ? 18 : 24) -
-        //           14}px`
-        //       },
-        //       ease: 'Power3.easeInOut'
-        //     }
-        //   },
-        //   0.2
-        // )
-        // await this.$delay((1.5 + 0.3 * 12) * 1000)
-        // GSAP.staggerTo(
-        //   '.Logo span',
-        //   isMobile ? 0.8 : 1.4,
-        //   {
-        //     top: isMobile ? 32 : '50%',
-        //     scale: 1,
-        //     opacity: 1,
-        //     rotationY: 0,
-        //     rotationZ: -360,
-        //     cycle: {
-        //       y: '0px',
-        //       z: '0px',
-        //       left: function(index) {
-        //         return `${index * 14 + 24}px`
-        //       },
-        //       ease: 'Power3.easeInOut'
-        //     }
-        //   },
-        //   0.05
-        // )
+        TweenMax.staggerFromTo(
+          '.Logo span',
+          1.5,
+          {
+            left: `${winWidth / 2}px`,
+            scale: 3,
+            opacity: 0,
+            rotationX: 180,
+            cycle: {
+              y: function(index) {
+                return `${Math.floor(Math.random() * 600 - 300)}px`
+              },
+              z: function(index) {
+                return `${winWidth - Math.floor(Math.random() * 40 - 20)}px`
+              },
+              left: function(index) {
+                return `${winWidth / 2 -
+                  Math.floor(Math.random() * 600 - 300)}px`
+              }
+            }
+          },
+          {
+            scale: 1,
+            opacity: 1,
+            rotationX: 0,
+            rotationY: 360,
+            rotationZ: 360,
+            cycle: {
+              y: '0px',
+              z: '0px',
+              left: function(index) {
+                return `${winWidth / 2 -
+                  168 / 2 +
+                  index * 14 +
+                  (isMobile ? 18 : 24) -
+                  14}px`
+              },
+              ease: 'Power3.easeInOut'
+            }
+          },
+          0.2
+        )
+        await this.$delay((1.5 + 0.3 * 12) * 1000)
+        TweenMax.staggerTo(
+          '.Logo span',
+          isMobile ? 0.8 : 1.4,
+          {
+            top: isMobile ? 32 : '50%',
+            scale: 1,
+            opacity: 1,
+            rotationY: 0,
+            rotationZ: -360,
+            cycle: {
+              y: '0px',
+              z: '0px',
+              left: function(index) {
+                return `${index * 14 + 24}px`
+              },
+              ease: 'Power3.easeInOut'
+            }
+          },
+          0.05
+        )
         // Curtain
         GSAP.to('.Curtain', isMobile ? 1.5 : 3, {
           x: '-10%',
