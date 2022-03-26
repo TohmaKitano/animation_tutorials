4c3871dbfb992ccc22177e8cecc0cad196953a65
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-26 15:29:37 +0900

    【Pending】set scroll event on the way

--- text_scroll_event/src/assets/js/index.js
+++ text_scroll_event/src/assets/js/index.js
@@ -3,6 +3,9 @@ import  '../scss/style.scss';
 window.addEventListener('DOMContentLoaded', () => {
   let $elem = document.querySelector('.titleeffect');
   let isVisible = $elem.classList.contains('titleeffect-visible');
+  console.log(isVisible);
+
+  // domを生成
   if (isVisible) {
     $elem.classList.add('titleeffect-animated');
     $elem.find(".titleeffect__clone").remove();
@@ -12,7 +15,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let text = $elem.textContent;
     let clone = '<span class="titleeffect__clone">'.concat(text, '</span>');
     let cover = '<span class="titleeffect__cover">'.concat(text, '</span>');
-    console.log(clone);
-    console.log(cover);
+    let copy = '<span class="titleeffect__df">'.concat(text, '</span>');
+    $elem.innerHTML = copy.concat(clone).concat(cover);
   }
+  console.log($elem);
+
 }, false)
