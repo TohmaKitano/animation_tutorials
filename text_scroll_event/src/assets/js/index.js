8a7d25d4e0eaa7b909d4857abf6bae8618f7c755
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-27 13:24:31 +0900

    【Stack】GSAP stuck during iterative process

--- text_scroll_event/src/assets/js/index.js
+++ text_scroll_event/src/assets/js/index.js
@@ -1,14 +1,15 @@
 import  '../scss/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
-  let titEffect = document.querySelector('.titEffect');
+  // Get DOM Element
+  let titEffect    = document.querySelector('.titEffect');
   let isTitVisible = titEffect.classList.contains('titEffect-visible');
-
-  creareNewTitEffectContent();  
-  animateNewTitEffectContent();
+    
+  creareNewTitEffectContent(isTitVisible, titEffect);
+  animateNewTitEffectContent(isTitVisible, titEffect);
 
   // Create New DOM Content
-  function creareNewTitEffectContent() {
+  function creareNewTitEffectContent(isTitVisible, titEffect) {
     if (isTitVisible) {
       titEffect.classList.add('titEffect-animated');
       titEffect.find('.titEffect__clone').remove();
@@ -24,27 +25,28 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   // Animate New DOM Content
-  function animateNewTitEffectContent () {
+  function animateNewTitEffectContent (isTitVisible, titEffect) {
     if (!isTitVisible) {
       let newTitEffectClone = document.querySelector('.titEffect__clone'),
           newTitEffectCover = document.querySelector('.titEffect__cover');
-      let elemHeight = titEffect.offsetHeight;
-      let elemWidth = titEffect.offsetWidth;
+      let elemHeight = titEffect.offsetHeight,
+          elemWidth  = titEffect.offsetWidth;
       let initialCloneRect  = 'rect(0px 0px '.concat(elemHeight, 'px 0px)'),
           archivedCloneRect = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px 0px)'),
           initialCoverRect  = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px 0px)'),
           archivedCoverRect = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px ').concat(elemWidth, "px)");
       newTitEffectClone.style.clip = initialCloneRect;
       newTitEffectCover.style.clip = initialCoverRect;
+      console.log(newTitEffectCover);
       titEffect.classList.add('titEffect-visible');
       gsap.to(newTitEffectClone, 1.5, {
         clip: archivedCloneRect,
-        ease: Power3.easeOut
+        ease: Power3.easeOut,
       })
       gsap.to(newTitEffectCover, 1.5, {
         clip: archivedCoverRect,
         ease: Power3.easeOut,
-        onComplete:function(){
+        onComplete:function() {
           titEffect.classList.add('titEffect-animated'),
           newTitEffectClone.remove(),
           newTitEffectCover.remove()
