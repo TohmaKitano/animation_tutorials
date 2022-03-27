43676aea55264e14bb7b9cf7e659f748e2860883
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-03-27 22:37:22 +0900

    change argement value

--- text_scroll_event/src/assets/js/index.js
+++ text_scroll_event/src/assets/js/index.js
@@ -9,43 +9,50 @@ window.addEventListener('DOMContentLoaded', () => {
   animateNewTitEffectContent(isTitVisible, titEffect);
 
   // Create New DOM Content
-  function creareNewTitEffectContent(isTitVisible, titEffect) {
-    if (isTitVisible) {
-      titEffect.classList.add('titEffect-animated');
-      titEffect.find('.titEffect__clone').remove();
-      titEffect.find('.titEffect__cover').remove();
+  function creareNewTitEffectContent(bool, el) {
+    if (bool) {
+      el.classList.add('titEffect-animated');
+      el.find('.titEffect__clone').remove();
+      el.find('.titEffect__cover').remove();
     }
     else {
-      let titEffectContent     = titEffect.textContent,
+      let titEffectContent     = el.textContent,
           titEffectClone       = '<span class="titEffect__clone">'.concat(titEffectContent, '</span>'),
           titEffectCover       = '<span class="titEffect__cover">'.concat(titEffectContent, '</span>'),
           titEffectDuplication = '<span class="titEffect__detail">'.concat(titEffectContent, '</span>');
-      titEffect.innerHTML = titEffectDuplication.concat(titEffectClone).concat(titEffectCover);
+      el.innerHTML = titEffectDuplication.concat(titEffectClone).concat(titEffectCover);
     }
   }
 
   // Animate New DOM Content
-  function animateNewTitEffectContent (isTitVisible, titEffect) {
-    if (!isTitVisible) {
+  function animateNewTitEffectContent (bool, el) {
+    if (!bool) {
       let newTitEffectClone = document.querySelector('.titEffect__clone'),
           newTitEffectCover = document.querySelector('.titEffect__cover');
-      let elemHeight = titEffect.offsetHeight,
-          elemWidth  = titEffect.offsetWidth;
+      let elemHeight = el.offsetHeight,
+          elemWidth  = el.offsetWidth;
       let initialCloneRect  = 'rect(0px 0px '.concat(elemHeight, 'px 0px)'),
           archivedCloneRect = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px 0px)'),
           initialCoverRect  = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px 0px)'),
           archivedCoverRect = 'rect(0px '.concat(elemWidth, 'px ').concat(elemHeight, 'px ').concat(elemWidth, "px)");
       newTitEffectClone.style.clip = initialCloneRect;
       newTitEffectCover.style.clip = initialCoverRect;
-      console.log(newTitEffectCover);
       titEffect.classList.add('titEffect-visible');
       gsap.to(newTitEffectClone, 1.5, {
         clip: archivedCloneRect,
         ease: Power3.easeOut,
+        scrollTrigger: {
+          trigger: titEffect,
+          start: 'top center'
+        }
       })
       gsap.to(newTitEffectCover, 1.5, {
         clip: archivedCoverRect,
         ease: Power3.easeOut,
+        scrollTrigger: {
+          trigger: titEffect,
+          start: 'top center'
+        },
         onComplete:function() {
           titEffect.classList.add('titEffect-animated'),
           newTitEffectClone.remove(),
