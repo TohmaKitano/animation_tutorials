901d63434872340427000c07f952ff5b9842960d
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-04-24 16:59:26 +0900

    orgaize canvas settings

--- mouse_stalker/src/assets/js/modules/oscillator.js
+++ mouse_stalker/src/assets/js/modules/oscillator.js
@@ -1,358 +1,298 @@
+// プロトタイプベース言語
+// オブジェクト指向プログラミング言語の1種で、変数（データ）やメソッドをクラスに依存せずに追加できるようにしたもののこと
+
 const initOscillator = (remove) => {
 
   if(!remove) {
 
-      var ctx,
-          hue,
-          logo,
-          form,
-          buffer,
-          target = {},
-          tendrils = [],
-          settings = {};
+    let ctx, hue, logo, form, buffer,
+        target   = {},
+        tendrils = [],
+        settings = {};
 
-      settings.debug = false;
-      settings.friction = 0.5;
-      settings.trails = 30;
-      settings.size = 50;
-      settings.dampening = 0.25;
-      settings.tension = 0.98;
+    settings.debug = false;
+    settings.friction = 0.5;
+    settings.trails = 30;
+    settings.size = 50;
+    settings.dampening = 0.25;
+    settings.tension = 0.98;
 
-      Math.TWO_PI = Math.PI * 2;
+    Math.TWO_PI = Math.PI * 2;
 
-      // ========================================================================================
-      // Oscillator
-      // ----------------------------------------------------------------------------------------
+    // Set Class Oscillator
+    function Oscillator(options) {
+      this.init(options || {});
+    }
+    Oscillator.prototype = (function() {
+      var value = 0;
+      return {
+        init: function(options) {
+          this.phase = options.phase || 0;
+          this.offset = options.offset || 0;
+          this.frequency = options.frequency || 0.001;
+          this.amplitude = options.amplitude || 1;
+        },
+        update: function() {
+          this.phase += this.frequency;
+          value = this.offset + Math.sin(this.phase) * this.amplitude;
+          return value;
+        },
+        value: function() {
+          return value;
+        }
+      };
+    })();
 
-      function Oscillator(options) {
-          this.init(options || {});
+    // Set Class Tendril
+    function Tendril(options) {
+      this.init(options || {});
+    }
+    Tendril.prototype = (function() {
+      function Node() {
+        this.x  = 0;
+        this.y  = 0;
+        this.vy = 0;
+        this.vx = 0;
       }
+      return {
+        init: function(options) {
+          this.spring   = options.spring + (Math.random() * 0.1) - 0.05;
+          this.friction = settings.friction + (Math.random() * 0.01) - 0.005;
+          this.nodes    = [];
 
-      Oscillator.prototype = (function () {
+          for (var i = 0, node; i < settings.size; i += 1) {
+            node   = new Node();
+            node.x = target.x;
+            node.y = target.y;
+            this.nodes.push(node);
+          }
+        },
+        update: function() {
+          let spring = this.spring,
+              node   = this.nodes[0];
 
-          var value = 0;
+          node.vx += (target.x - node.x) * spring;
+          node.vy += (target.y - node.y) * spring;
 
-          return {
+          for (var prev, i = 0, n = this.nodes.length; i < n; i += 1) {
+            node = this.nodes[i];
+            if (i > 0) {
+              prev    = this.nodes[i - 1];
+              node.vx += (prev.x - node.x) * spring;
+              node.vy += (prev.y - node.y) * spring;
+              node.vx += prev.vx * settings.dampening;
+              node.vy += prev.vy * settings.dampening;
+            }
+            node.vx *= this.friction;
+            node.vy *= this.friction;
+            node.x  += node.vx;
+            node.y  += node.vy;
+            spring  *= settings.tension;
+          }
+        },
+        draw: function() {
+          var x = this.nodes[0].x,
+              y = this.nodes[0].y,
+              a, b;
 
-              init: function (options) {
-                  this.phase = options.phase || 0;
-                  this.offset = options.offset || 0;
-                  this.frequency = options.frequency || 0.001;
-                  this.amplitude = options.amplitude || 1;
-              },
+          ctx.beginPath();
+          ctx.moveTo(x, y);
 
-              update: function () {
-                  this.phase += this.frequency;
-                  value = this.offset + Math.sin(this.phase) * this.amplitude;
-                  return value;
-              },
-
-              value: function () {
-                  return value;
-              }
-          };
-
-      })();
-
-      // ========================================================================================
-      // Tendril
-      // ----------------------------------------------------------------------------------------
-
-      function Tendril(options) {
-          this.init(options || {});
-      }
-
-      Tendril.prototype = (function () {
-
-          function Node() {
-              this.x = 0;
-              this.y = 0;
-              this.vy = 0;
-              this.vx = 0;
+          for (var i = 1, n = this.nodes.length - 2; i < n; i += 1) {
+            a = this.nodes[i];
+            b = this.nodes[i + 1];
+            x = (a.x + b.x) * 0.5;
+            y = (a.y + b.y) * 0.5;
+            ctx.quadraticCurveTo(a.x, a.y, x, y);
           }
 
-          return {
+          a = this.nodes[i];
+          b = this.nodes[i + 1];
 
-              init: function (options) {
+          ctx.quadraticCurveTo(a.x, a.y, b.x, b.y);
+          ctx.stroke();
+          ctx.closePath();
+        }
+      };
+    })();
 
-                  this.spring = options.spring + (Math.random() * 0.1) - 0.05;
-                  this.friction = settings.friction + (Math.random() * 0.01) - 0.005;
-                  this.nodes = [];
 
-                  for (var i = 0, node; i < settings.size; i++) {
+    function init(event) {
+      document.removeEventListener('mousemove', init);
+      document.removeEventListener('touchstart', init);
+      document.addEventListener('mousemove', mousemove);
+      document.addEventListener('touchmove', mousemove);
+      document.addEventListener('touchstart', touchstart);
+      mousemove(event);
+      reset();
+      loop();
+    }
 
-                      node = new Node();
-                      node.x = target.x;
-                      node.y = target.y;
+    function reset() {
+      tendrils = [];
+      for (var i = 0; i < settings.trails; i++) {
+        tendrils.push(new Tendril({
+          spring: 0.45 + 0.025 * (i / settings.trails)
+        }));
+      }
+    }
 
-                      this.nodes.push(node);
-                  }
-              },
+    // Random Color
+    function randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    let color = randomIntFromInterval(1, 2);
 
-              update: function () {
+    function loop() {
+      if (!ctx.running) return;
+      
+      // Canvas setting
+      ctx.globalCompositeOperation = 'source-over';
+      ctx.fillStyle = '#ffffff';
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.globalCompositeOperation = 'xor';
+      ctx.strokeStyle = 'hsla(190, 100%, 50%, 0.25)';
+      ctx.lineWidth = 1;
 
-                  var spring = this.spring,
-                      node = this.nodes[0];
+      if (color === 1) ctx.strokeStyle = 'hsla(190, 100%, 50%, 0.25)';
+      else ctx.strokeStyle = 'hsla(340, 100%, 50%, 0.25)';
 
-                  node.vx += (target.x - node.x) * spring;
-                  node.vy += (target.y - node.y) * spring;
-
-                  for (var prev, i = 0, n = this.nodes.length; i < n; i++) {
-
-                      node = this.nodes[i];
-
-                      if (i > 0) {
-
-                          prev = this.nodes[i - 1];
-
-                          node.vx += (prev.x - node.x) * spring;
-                          node.vy += (prev.y - node.y) * spring;
-                          node.vx += prev.vx * settings.dampening;
-                          node.vy += prev.vy * settings.dampening;
-                      }
-
-                      node.vx *= this.friction;
-                      node.vy *= this.friction;
-                      node.x += node.vx;
-                      node.y += node.vy;
-
-                      spring *= settings.tension;
-                  }
-              },
-
-              draw: function () {
-
-                  var x = this.nodes[0].x,
-                      y = this.nodes[0].y,
-                      a, b;
-
-                  ctx.beginPath();
-                  ctx.moveTo(x, y);
-
-                  for (var i = 1, n = this.nodes.length - 2; i < n; i++) {
-
-                      a = this.nodes[i];
-                      b = this.nodes[i + 1];
-                      x = (a.x + b.x) * 0.5;
-                      y = (a.y + b.y) * 0.5;
-
-                      ctx.quadraticCurveTo(a.x, a.y, x, y);
-                  }
-
-                  a = this.nodes[i];
-                  b = this.nodes[i + 1];
-
-                  ctx.quadraticCurveTo(a.x, a.y, b.x, b.y);
-                  ctx.stroke();
-                  ctx.closePath();
-              }
-          };
-
-      })();
-
-      // ----------------------------------------------------------------------------------------
-
-      function init(event) {
-
-          document.removeEventListener('mousemove', init);
-          document.removeEventListener('touchstart', init);
-
-          document.addEventListener('mousemove', mousemove);
-          document.addEventListener('touchmove', mousemove);
-          document.addEventListener('touchstart', touchstart);
-
-          mousemove(event);
-          reset();
-          loop();
+      for (var i = 0, tendril; i < settings.trails; i += 1) {
+        tendril = tendrils[i];
+        tendril.update();
+        tendril.draw();
       }
 
-      function reset() {
+      ctx.frame++;
+      requestAnimFrame(loop);
+    }
 
-          tendrils = [];
+    function resize() {
+      ctx.canvas.width  = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
+    }
 
-          for (var i = 0; i < settings.trails; i++) {
-
-              tendrils.push(new Tendril({
-                  spring: 0.45 + 0.025 * (i / settings.trails)
-              }));
-          }
+    function start() {
+      if (!ctx.running) {
+        ctx.running = true;
+        loop();
       }
+    }
 
+    function stop() {
+      ctx.running = false;
+    }
 
-      function randomIntFromInterval(min, max) { // min and max included
-          return Math.floor(Math.random() * (max - min + 1) + min);
+    function mousemove(event) {
+      if (event.touches) {
+        target.x = event.touches[0].pageX;
+        target.y = event.touches[0].pageY;
+      } else {
+        target.x = event.clientX
+        target.y = event.clientY;
       }
+      event.preventDefault();
+    }
 
-      var color = randomIntFromInterval(1, 2);
-
-      function loop() {
-
-          if (!ctx.running) return;
-
-          ctx.globalCompositeOperation = 'source-over';
-          ctx.fillStyle = '#ffffff';
-          ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-          ctx.globalCompositeOperation = 'xor';
-          ctx.strokeStyle = 'hsla(346,98%,56%,0.25)';
-          ctx.lineWidth = 1;
-
-
-          if (color == 1) {
-              ctx.strokeStyle = 'hsla(346,98%,56%,0.25)';
-          } else {
-              ctx.strokeStyle = 'hsla(171,98%,56%,0.25)';
-          }
-
-
-          for (var i = 0, tendril; i < settings.trails; i++) {
-              tendril = tendrils[i];
-              tendril.update();
-              tendril.draw();
-          }
-
-          ctx.frame++;
-          requestAnimFrame(loop);
+    function touchstart(event) {
+      if (event.touches.length == 1) {
+        target.x = event.touches[0].pageX;
+        target.y = event.touches[0].pageY;
       }
+    }
 
-      function resize() {
-          ctx.canvas.width = window.innerWidth;
-          ctx.canvas.height = window.innerHeight;
-      }
+    // function keyup(event) {
+    //   switch (event.keyCode) {
+    //     case 32:
+    //       save();
+    //       break;
+    //     default:
+    //   }
+    // }
 
-      function start() {
-          if (!ctx.running) {
-              ctx.running = true;
-              loop();
-          }
-      }
+    // function letters(id) {
+    //   let el = document.getElementById(id),
+    //       letters = el.innerHTML.replace('&amp;', '&').split(''),
+    //       heading = '';
 
-      function stop() {
-          ctx.running = false;
-      }
+    //   for (let i = 0, n = letters.length, letter; i < n; i += 1) {
+    //     letter = letters[i].replace('&', '&amp');
+    //     heading += letter.trim() ? '<span class="letter-' + i + '">' + letter + '</span>' : '&nbsp;';
+    //   }
 
-      function mousemove(event) {
-          if (event.touches) {
-              target.x = event.touches[0].pageX;
-              target.y = event.touches[0].pageY;
-          } else {
-              target.x = event.clientX
-              target.y = event.clientY;
-          }
-          event.preventDefault();
-      }
+    //   el.innerHTML = heading;
+    //   setTimeout(function() {
+    //       el.className = 'transition-in';
+    //     }, (Math.random() * 500) + 500);
+    // }
 
-      function touchstart(event) {
-          if (event.touches.length == 1) {
-              target.x = event.touches[0].pageX;
-              target.y = event.touches[0].pageY;
-          }
-      }
+    // function save() {
+    //   if (!buffer) {
+    //     buffer = document.createElement('canvas');
+    //     buffer.width = screen.availWidth;
+    //     buffer.height = screen.availHeight;
+    //     buffer.ctx = buffer.getContext('2d');
 
-      function keyup(event) {
+    //     form = document.createElement('form');
+    //     form.method = 'post';
+    //     form.input = document.createElement('input');
+    //     form.input.type = 'hidden';
+    //     form.input.name = 'data';
+    //     form.appendChild(form.input);
 
-          switch (event.keyCode) {
-              case 32:
-                  save();
-                  break;
-              default:
-              // console.log(event.keyCode);
-          }
-      }
+    //     document.body.appendChild(form);
+    //   }
 
-      function letters(id) {
+    //   buffer.ctx.fillStyle = 'rgba(8,5,16)';
+    //   buffer.ctx.fillRect(0, 0, buffer.width, buffer.height);
 
-          var el = document.getElementById(id),
-              letters = el.innerHTML.replace('&amp;', '&').split(''),
-              heading = '';
+    //   buffer.ctx.drawImage(canvas,
+    //     Math.round(buffer.width / 2 - canvas.width / 2),
+    //     Math.round(buffer.height / 2 - canvas.height / 2)
+    //   );
 
-          for (var i = 0, n = letters.length, letter; i < n; i++) {
-              letter = letters[i].replace('&', '&amp');
-              heading += letter.trim() ? '<span class="letter-' + i + '">' + letter + '</span>' : '&nbsp;';
-          }
+    //   window.open(buffer.toDataURL(), 'wallpaper', 'top=0,left=0,width=' + buffer.width + ',height=' + buffer.height);
 
-          el.innerHTML = heading;
-          setTimeout(function () {
-              el.className = 'transition-in';
-          }, (Math.random() * 500) + 500);
-      }
+    //   // form.input.value = buffer.toDataURL().substr(22);
+    //   // form.submit();
+    // }
 
-      function save() {
+    window.requestAnimFrame = (function() {
+      return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (fn) {
+        window.setTimeout(fn, 1000 / 60)
+      };
+    })();
 
-          if (!buffer) {
+    ctx = document.getElementById('canvas').getContext('2d');
 
-              buffer = document.createElement('canvas');
-              buffer.width = screen.availWidth;
-              buffer.height = screen.availHeight;
-              buffer.ctx = buffer.getContext('2d');
+    ctx.running = true;
+    ctx.frame = 1;
 
-              form = document.createElement('form');
-              form.method = 'post';
-              form.input = document.createElement('input');
-              form.input.type = 'hidden';
-              form.input.name = 'data';
-              form.appendChild(form.input);
+    hue = new Oscillator({
+      phase: Math.random() * Math.TWO_PI,
+      amplitude: 85,
+      frequency: 0.0015,
+      offset: 285
+    });
 
-              document.body.appendChild(form);
-          }
+    document.addEventListener('mousemove', init);
+    document.addEventListener('touchstart', init);
+    document.body.addEventListener('orientationchange', resize);
+    window.addEventListener('resize', resize);
+    window.addEventListener('focus', start);
+    window.addEventListener('blur', stop);
 
-          buffer.ctx.fillStyle = 'rgba(8,5,16)';
-          buffer.ctx.fillRect(0, 0, buffer.width, buffer.height);
-
-          buffer.ctx.drawImage(canvas,
-              Math.round(buffer.width / 2 - canvas.width / 2),
-              Math.round(buffer.height / 2 - canvas.height / 2)
-          );
-
-
-          window.open(buffer.toDataURL(), 'wallpaper', 'top=0,left=0,width=' + buffer.width + ',height=' + buffer.height);
-
-          // form.input.value = buffer.toDataURL().substr(22);
-          // form.submit();
-      }
-
-      window.requestAnimFrame = (function () {
-          return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (fn) {
-              window.setTimeout(fn, 1000 / 60)
-          };
-      })();
-
-
-      ctx = document.getElementById('canvas').getContext('2d');
-      console.log(ctx)
-
-      ctx.running = true;
-      ctx.frame = 1;
-
-
-      hue = new Oscillator({
-          phase: Math.random() * Math.TWO_PI,
-          amplitude: 85,
-          frequency: 0.0015,
-          offset: 285
-      });
-
-
-      document.addEventListener('mousemove', init);
-      document.addEventListener('touchstart', init);
-      document.body.addEventListener('orientationchange', resize);
-      window.addEventListener('resize', resize);
-      window.addEventListener('focus', start);
-      window.addEventListener('blur', stop);
-
-      resize();
+    resize();
 
   } else {
 
-
-      document.body.removeEventListener('orientationchange', resize);
-      window.removeEventListener('resize', resize);
-      window.removeEventListener('focus', start);
-      window.removeEventListener('blur', stop);
-
-      document.removeEventListener('mousemove', mousemove);
-      document.removeEventListener('touchmove', mousemove);
-      document.removeEventListener('touchstart', touchstart);
-
+    document.body.removeEventListener('orientationchange', resize);
+    window.removeEventListener('resize', resize);
+    window.removeEventListener('focus', start);
+    window.removeEventListener('blur', stop);
+    document.removeEventListener('mousemove', mousemove);
+    document.removeEventListener('touchmove', mousemove);
+    document.removeEventListener('touchstart', touchstart);
 
   }
 }
